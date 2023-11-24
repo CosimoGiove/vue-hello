ef2deb70e7754d4b455ba09efb724278453c825f
cosimogiove <cosimo18497@gmail.com>
2023-11-24 15:20:17 +0100

    feat : esercizio finito +bonus

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { createApp } = Vue
 createApp({
  data() {
     return {
-       messaggioBenvenuto: "il mio primo titolo scritto in vu-js",         immagine: "https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699401600&semt=sph",
+       messaggioBenvenuto: "il mio primo titolo scritto in vue-js",         immagine: "https://img.freepik.com/free-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_335224-794.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699401600&semt=sph",
        alt: "immagine paesaggio con mongolfiera",
        h1Style: "color",
        imgStyle: "imgstyle",
@@ -15,5 +15,5 @@ createApp({
        descrizione: "descrizione",
 
         }
-    }
+    },
 }).mount('#app')
